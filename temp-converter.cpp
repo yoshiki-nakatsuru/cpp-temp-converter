bfcfdae4d017ff18689b01600613efa8f849b9a7
Lewis Baker <68536803+woven-baker@users.noreply.github.com>
2023-10-13 14:01:50 +0900

    Create temp-converter.cpp

---
+++ temp-converter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void convertCelsiusToFahrenheit() {
+    std::cout << "Enter the temperature in Celsius: ";
+    double celsius;
+    std::cin >> celsius;
+
+    double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+    
+    std::cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit." << std::endl;
+}
+
+int main() {
+    return 0;
+}
